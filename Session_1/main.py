ea6057d0b2184c83917ba3ba0355be7b2bebbff3
Alejandro Ospina <nano.santosm@gmail.com>
2026-04-28 20:57:56 +0200

    feat- modify test fo r tiqui

--- Session_1/main.py
+++ Session_1/main.py
@@ -1,2 +1,2 @@
-hello_world(name="John Smith")  # Hello world, my name is John Smith and I'm learning about version control!
+hello_world(name="Alejandro")  # Hello world, my name is John Smith and I'm learning about version control!
 
